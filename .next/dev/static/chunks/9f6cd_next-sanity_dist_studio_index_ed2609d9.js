c7ee6cef96bfe021075890fc1f103dd276f951c9
TARIFEATTAR <jordan@asala.ai>
2026-05-04 02:49:18 -0700

    chore: stop tracking generated Next artifacts

--- .next/dev/static/chunks/9f6cd_next-sanity_dist_studio_index_ed2609d9.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9f6cd_next-sanity_dist_42fe21fc._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/c141e_refractor_lang_bash_9bf8c2ff.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/c141e_refractor_lang_bash_f583face.js"
-],
-    source: "dynamic"
-});
